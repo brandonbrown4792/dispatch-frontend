61e16cfa8e38ff7a143f0915c0a419316a5c0469
github username <github email address>
2020-06-15 23:01:51 -0400

    Added filter by nurse functionality

--- src/Components/FilterContainer.js
+++ src/Components/FilterContainer.js
@@ -1,19 +1,25 @@
 import React from 'react'
-import Filter from './Filter'
+// import Filter from './Filter'
+
+const updateFilteredNurse = (e, filterParams, updateFilteredUserData) => {
+    updateFilteredUserData({ ...filterParams, nurse: e.target.value })
+}
+
+const mapNursesOptions = nurses => {
+    if (nurses) {
+        return nurses.map(nurse => <option key={nurse.id} value={nurse.id}>{nurse.name}</option>)
+    }
+}
 
 function FilterContainer(props) {
-    // Map over props.filterTypes to create array of <Filter /> components with labels == filterTypes
-
-    // let filterMap = props.options.map(option => <Filter option={option} /> )
-    // need to assign a unique ID to each filter so we can use it in it's htmlFor label later
     return (
         <div>
             <h3>Filters:</h3>
-            <Filter />
-            <Filter />
-            <Filter />
-            <Filter />
-            {/* {filterMap(props)} */}
+            <label>Nurse</label><br />
+            <select onChange={e => updateFilteredNurse(e, props.filterParams, props.updateFilteredUserData)}>
+                <option value=''>All Nurses</option>
+                {mapNursesOptions(props.userData.nurses)}
+            </select>
         </div>
     )
 }
